57c0ada493b66d995821454fc8c5a11f322dedd2
macovei-alexandru <maccelmare@gmail.com>
2023-10-26 14:41:50 +0300

    Fixed the project filters

--- Client/Game.h
+++ Client/Game.h
@@ -2,13 +2,19 @@
 #include <iostream>
 #include <vector>
 #include <string>
+
+#include "Turn.h"
+
 class Game
 {
 public:
+
 	void start();
 	void pause();
 	void removePlayer();
+
 private:
+
 	std::vector<Player> m_players;
 	uint16_t m_roundNo;
 	uint16_t m_playerToDrawID;
@@ -16,5 +22,6 @@ private:
 	std::vector<const std::string&> m_shownWords;
 	std::vector<GameSettings> m_gameSettings;
 	Turn m_turn;
+
 };
 
