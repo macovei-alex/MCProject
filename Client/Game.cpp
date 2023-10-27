0c2904d6849ae2ca7c52689ccc9dcaa60fa0da47
AndreiCIlies <116495791+AndreiCIlies@users.noreply.github.com>
2023-10-27 09:20:20 +0300

    Working on class Game

--- Client/Game.cpp
+++ Client/Game.cpp
@@ -1,1 +1,49 @@
 #include "Game.h"
+
+void Game::start()
+{
+}
+
+void Game::pause()
+{
+}
+
+void Game::removePlayer()
+{
+}
+
+Game::Game(std::vector<Player> players, uint16_t roundNo, uint16_t playerToDrawID, 
+	uint16_t ownerID, std::vector<const std::string&> shownWords, Turn turn):
+	m_players(players),
+	m_roundNo(roundNo),
+	m_playerToDrawID(playerToDrawID),
+	m_ownerID(ownerID),
+	m_shownWords(shownWords),
+	/*m_gameSettings(gameSettings)*/
+	m_turn(turn)
+{}
+
+Game::Game(const Game& game):
+	m_players(game.m_players),
+	m_roundNo(game.m_roundNo),
+	m_playerToDrawID(game.m_playerToDrawID),
+	m_ownerID(game.m_ownerID),
+	m_shownWords(game.m_shownWords),
+	/*m_gameSettings(game.m_gameSettings)*/
+	m_turn(game.m_turn)
+{}
+
+Game::~Game()
+{
+}
+
+Game& Game::operator=(const Game& game)
+{
+	m_players = game.m_players;
+	m_roundNo = game.m_roundNo;
+	m_playerToDrawID = game.m_playerToDrawID;
+	m_ownerID = game.m_ownerID;
+	m_shownWords = game.m_shownWords;
+	m_turn = game.m_turn;
+	return *this;
+}
--- Client/Game.h
+++ Client/Game.h
@@ -2,7 +2,7 @@
 #include <iostream>
 #include <vector>
 #include <string>
-
+#include "Player.h"
 #include "Turn.h"
 
 class Game
@@ -12,6 +12,14 @@ public:
 	void start();
 	void pause();
 	void removePlayer();
+	Game(std::vector<Player> players, uint16_t roundNo, uint16_t playerToDrawID,
+		uint16_t ownerID,
+		std::vector<const std::string&> shownWords,
+		/*std::vector<GameSettings> gameSettings;*/
+		Turn turn);
+	Game(const Game& game);
+	~Game();
+	Game& operator=(const Game& game);
 
 private:
 
@@ -20,8 +28,6 @@ private:
 	uint16_t m_playerToDrawID;
 	uint16_t m_ownerID;
 	std::vector<const std::string&> m_shownWords;
-	std::vector<GameSettings> m_gameSettings;
+	// std::vector<GameSettings> m_gameSettings;
 	Turn m_turn;
-
 };
-
