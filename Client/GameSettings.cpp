3feae07be10ac8c43d65fb7e19d21726a2480105
Dutu Andrada <dutuandrada14@gmail.com>
2023-10-27 23:48:46 +0300

    added Getters

--- Client/GameSettings.cpp
+++ Client/GameSettings.cpp
@@ -8,3 +8,18 @@ void GameSettings::chooseSettings()
 {
 	//EMPTY
 }
+
+uint8_t GameSettings::GetDrawTime() const
+{
+	return m_drawTime;
+}
+
+uint8_t GameSettings::GetRoundCount() const
+{
+	return m_roundCount;
+}
+
+uint8_t GameSettings::GetChooseWordOptionCount() const
+{
+	return m_chooseWordOptionCount;
+}
--- Client/GameSettings.h
+++ Client/GameSettings.h
@@ -6,12 +6,16 @@ public:
 
 	GameSettings();
 	void chooseSettings();
+	uint8_t GetDrawTime() const;
+	uint8_t GetRoundCount() const;
+	uint8_t GetChooseWordOptionCount() const;
+
 
 private:
 
 	 uint8_t m_roundCount;
 	 uint8_t m_drawTime;
-	 uint8_t chooseWordOptionCount;
+	 uint8_t m_chooseWordOptionCount;
 	 // std::string customWords;
 	 static int const defaultRoundCount = 0;
 	 static int const defaultDrawTime = 80;
