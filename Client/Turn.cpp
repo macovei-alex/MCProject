b95dc4ac3e8a8cdd1c7bd2a09f1d819e832cb071
macovei-alexandru <maccelmare@gmail.com>
2023-10-31 19:13:53 +0200

    word generation and linking to Player logic started

--- Client/Turn.cpp
+++ Client/Turn.cpp
@@ -1,46 +1,38 @@
 #include "Turn.h"
-#include <iostream>
-#include <limits>
-#include <array>
 
 const std::string Turn::wordsFilePath = "words.in";
-std::random_device Turn::seed; 
-std::mt19937 Turn::randomNumberGenerator(seed());
 
-Turn::Turn(uint8_t turnNumber, const std::string& word, int8_t turnToDraw) :
+Turn::Turn(uint8_t turnNumber) :
 	m_turnNumber{ turnNumber },
-	m_word{ word },
-	m_showLetterIDs{},
-	m_turnToDraw{ turnToDraw }
+	m_word{},
+	m_showLetterIDs{}
 {
 	m_chooseWordStartTime = clock();
 	m_startTime = std::numeric_limits<double>::max();
 	m_fin = std::ifstream(wordsFilePath);
 }
 
-Turn::Turn(Turn& turn) :
+Turn::Turn(const Turn& turn) :
 	m_turnNumber{ turn.m_turnNumber },
 	m_word{ turn.m_word },
 	m_showLetterIDs{ turn.m_showLetterIDs },
-	m_turnToDraw{ turn.m_turnToDraw },
 	m_startTime{ turn.m_startTime },
 	m_chooseWordStartTime{ turn.m_chooseWordStartTime },
 	m_fin{ wordsFilePath }
 {
-	m_fin.seekg(turn.m_fin.tellg());
+	// m_fin.seekg(turn.m_fin.tellg());
 }
 
-Turn& Turn::operator=(Turn& turn)
+Turn& Turn::operator=(const Turn& turn)
 {
 	m_turnNumber = turn.m_turnNumber;
 	m_word = turn.m_word;
 	m_showLetterIDs = turn.m_showLetterIDs;
-	m_turnToDraw = turn.m_turnToDraw;
 	m_startTime = turn.m_startTime;
 	m_chooseWordStartTime = turn.m_chooseWordStartTime;
 
 	m_fin = std::ifstream(wordsFilePath);
-	m_fin.seekg(turn.m_fin.tellg());
+	// m_fin.seekg(turn.m_fin.tellg());
 
 	return *this;
 }
@@ -55,14 +47,14 @@ uint8_t Turn::GetTurnNumber() const
 	return m_turnNumber;
 }
 
-double_t Turn::GetChooseTime() const
+float_t Turn::GetChoiceTime() const
 {
-	return double_t(clock() - m_chooseWordStartTime) / CLOCKS_PER_SEC;
+	return float_t(clock() - m_chooseWordStartTime) / CLOCKS_PER_SEC;
 }
 
-double_t Turn::GetPlayTime() const
+float_t Turn::GetPlayTime() const
 {
-	return double_t(clock() - m_startTime) / CLOCKS_PER_SEC;
+	return float_t(clock() - m_startTime) / CLOCKS_PER_SEC;
 }
 
 std::string Turn::GetWord() const
@@ -70,29 +62,42 @@ std::string Turn::GetWord() const
 	return m_word;
 }
 
-std::vector<std::string> Turn::generateWordChoices(const size_t wordsCount)
+std::vector<std::string> Turn::generateWordChoices(const uint8_t wordsCount)
 {
 	std::vector<std::string> words(wordsCount);
-	for(uint8_t i = 0; i < wordsCount && !m_fin.eof(); i++)
+	for (uint8_t i = 0; i < wordsCount && !m_fin.eof(); i++)
 	{
 		m_fin >> words[i];
 	}
 	return words;
 }
 
-std::string Turn::pickWord(const std::vector<std::string>& words, const size_t wordsCount) const
+void Turn::sendWordChoices(const Player& player, const std::vector<std::string>& wordChoices) const
 {
-	// random word choice
-	std::uniform_int_distribution<> distribution(0, wordsCount - 1);
-	return words[distribution(randomNumberGenerator)];
+	// [send] wordChoices [to] player
+}
+
+void Turn::choosingWordPhase(const std::vector<Player>& players)
+{
+	//std::vector<std::string> wordChoices = generateWordChoices(GameSettings::chooseWordOptionCount);
+	//int i;
+	//for (i = 0; i < players.size(); i++)
+	//	if (players[i].getRole() == Player::Role::Draw)
+	//	{
+	//		sendWordChoices(players[i], wordChoices);
+	//		break;
+	//	}
+	//
+	//while (GetChooseTime() < GameSettings::chooseWordTime)
+	//	// [check] receivedWordChoice [from] player
 }
 
 void Turn::startNewTurn(std::vector<Player>& players)
 {
-	m_word = "";
+	choosingWordPhase(players);
 	for (auto& player : players)
 	{
-		//player.resetCurrentScore();
+		// player.resetCurrentScore();
 	}
 	m_startTime = clock();
 }
@@ -103,4 +108,11 @@ void Turn::endTurn(std::vector<Player>& players)
 	{
 		player.addScore();
 	}
+}
+
+void Turn::startNextTurn(std::vector<Player>& players)
+{
+	endTurn(players);
+	m_turnNumber++;
+	startNewTurn(players);
 }
--- Client/Turn.h
+++ Client/Turn.h
@@ -1,39 +1,39 @@
 #pragma once
+
+#include "Player.h"
+#include "GameSettings.h"
+
 #include <cstdint>
 #include <string>
 #include <vector>
 #include <ctime>
 #include <fstream>
-#include <random>
-#include "Player.h"
 
 class Turn
 {
 public:
-	Turn(uint8_t turnNo, const std::string& word, int8_t turnToDraw);
-	Turn(Turn& turn);
-	Turn& operator=(Turn& turn);
+	Turn(uint8_t turnNumber);
+	Turn(const Turn& turn);
+	Turn& operator=(const Turn& turn);
 	~Turn();
 	uint8_t GetTurnNumber() const;
-	double_t GetChooseTime() const;
-	double_t GetPlayTime() const;
+	float_t GetChoiceTime() const;
+	float_t GetPlayTime() const;
 	std::string GetWord() const;
-	std::vector<std::string> generateWordChoices(const size_t wordsCount);
-	std::string pickWord(const std::vector<std::string>& words, const size_t wordsCount) const;
+	std::vector<std::string> generateWordChoices(const uint8_t wordsCount);
+	void sendWordChoices(const Player& player, const std::vector<std::string>& wordChoices) const;
+	void choosingWordPhase(const std::vector<Player>& players);
 	void startNewTurn(std::vector<Player>& players);
 	void endTurn(std::vector<Player>& players);
+	void startNextTurn(std::vector<Player>& players);
 
 	static const std::string wordsFilePath;
 
 private:
 	uint8_t m_turnNumber;
 	std::string m_word;
-	int8_t m_turnToDraw;
 	std::vector<uint8_t> m_showLetterIDs;
 	clock_t m_startTime;
 	clock_t m_chooseWordStartTime;
 	std::ifstream m_fin;
-
-	static std::random_device seed;
-	static std::mt19937 randomNumberGenerator;
 };
