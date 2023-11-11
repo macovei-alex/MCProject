bef78ace4ddba33995a5b22bda0b666b7a1f8b74
macovei-alexandru <maccelmare@gmail.com>
2023-11-11 20:48:45 +0200

    server-client connection achieved

--- ConsoleClient/ConsoleClient.cpp
+++ ConsoleClient/ConsoleClient.cpp
@@ -1,7 +1,23 @@
 #include <iostream>
+#include <string>
+
+#include <cpr/cpr.h>
+#include <crow.h>
 
 int main()
 {
+	while (true)
+	{
+		std::string message;
+		std::cin >> message;
+		if (message == "q")
+			break;
+
+		auto response = cpr::Get(
+			cpr::Url{ "http://localhost:18080/inputServer/" },
+			cpr::Parameters{ {"message", message} }
+		);
+	}
 
 	return 0;
 }
--- Server/Server.cpp
+++ Server/Server.cpp
@@ -1,4 +1,8 @@
 #include <iostream>
+#include <vector>
+
+#include <crow.h>
+#include <sqlite_orm/sqlite_orm.h>
 
 #include "Game.h"
 
@@ -6,5 +10,31 @@
 
 int main()
 {
+	std::vector<std::string> chat;
+
+	crow::SimpleApp app;
+
+	CROW_ROUTE(app, "/")([]() {
+		return "Test connection succesful\n";
+		});
+
+	CROW_ROUTE(app, "/inputServer/")([&chat](const crow::request& request) {
+		char* message = request.url_params.get("message");
+		if (!message)
+		{
+			std::cout << "Teapa cumetre\n";
+			return crow::response(404);
+		}
+		chat.push_back(std::string{ message });
+
+		for (const auto& message : chat) {
+			std::cout << message << '\n';
+		}
+
+		return crow::response(200);
+		});
+
+	app.port(18080).multithreaded().run();
+
 	return 0;
 }
