21be696ac8fd0c713e05a4aeffcc60e0217334ac
Dutu Andrada <dutuandrada14@gmail.com>
2023-11-19 13:45:32 +0200

    Added rule of 3 for Authentification class

--- Server/Authentification.cpp
+++ Server/Authentification.cpp
@@ -1,21 +1,27 @@
 #include "Authentification.h"
-
 crow::response Autentificare::Register(std::string username, std::string password)
 {
     return crow::response();
 }
-
 crow::response Autentificare::Login(std::string username, std::string password)
 {    auto player = m_dataBase.get_all<Player>(sql::where(sql::is_equal(&Player::playerName, username)));
      auto playerPassword = m_dataBase.get_all<Player>(sql::where(sql::is_equal(&Player::password, password)));
      if (player.size() == 0 || playerPassword.size() == 0)
      {
-		 return crow::response(404);
+		 return crow::response(404,"login failed");
 	 }
      else
      {
-         return crow::response(200); 
+         return crow::response(200,"login succesful");
      }
      
      return crow::response();
+}
+Autentificare::Autentificare(Storage& dataBase): m_dataBase(dataBase)
+{
+	/*EMPTY*/
+}
+Autentificare::~Autentificare()
+{ 
+   /*EMPTY*/
 }
--- Server/Authentification.h
+++ Server/Authentification.h
@@ -3,8 +3,13 @@
 #include "database.h"
 class Autentificare {
 public:
+	Autentificare(Storage& dataBase);
+	~Autentificare();
+	Autentificare( const Autentificare& autentificare ) = delete;
+	Autentificare& operator=( const Autentificare& ) = delete;
 	crow::response Register(std::string username, std::string password);
 	crow::response Login(std::string username, std::string password);
+	
 private:
 	Storage& m_dataBase;
 };
