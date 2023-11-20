017fe5c51898a864dae0ae265130f4e67482d15a
macovei-alexandru <maccelmare@gmail.com>
2023-11-20 15:23:10 +0200

    factory pattern for Server class

--- Server/Server.h
+++ Server/Server.h
@@ -11,13 +11,20 @@ public:
 	Server(const Server&) = delete;
 	Server& operator=(const Server&) = delete;
 	~Server() = default;
-	void run();
 
-private:
-	const uint16_t k_port = 18080;
+	Server& allHandlers();
+	Server& testHandlers();
+	Server& chatHandlers();
+	Server& roomHandlers();
+
+	Server& port(uint16_t port);
+
+	void run();
 
 private:
 	crow::SimpleApp m_app;
 	std::vector<utils::Message> m_chat;
 	utils::Lobby m_lobbyState;
+
+	uint16_t m_port;
 };
