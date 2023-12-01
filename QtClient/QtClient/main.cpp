64877f35d744b5fd2eed824a624a3a241b209e20
macovei-alexandru <maccelmare@gmail.com>
2023-12-01 18:57:27 +0200

    qt image now working

--- QtClient/QtClient/main.cpp
+++ QtClient/QtClient/main.cpp
@@ -1,11 +1,20 @@
 #include "mainwindow.h"
+#include <windows.h>
 
 #include <QApplication>
 
-int main(int argc, char *argv[])
+int main(int argc, char* argv[])
 {
-    QApplication a(argc, argv);
-    MainWindow w;
-    w.show();
-    return a.exec();
+	// Find out the current directory
+	TCHAR buffer[256];
+	DWORD buffer_size = MAX_PATH;
+	DWORD length = GetCurrentDirectory(buffer_size, buffer);
+	std::wstring wstr{ buffer, length };
+	qDebug() << "*****FOLDER***** : " << QString::fromStdWString(wstr);
+
+
+	QApplication a(argc, argv);
+	MainWindow w;
+	w.show();
+	return a.exec();
 }
