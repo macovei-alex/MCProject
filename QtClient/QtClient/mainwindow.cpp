13d33a198a52f53191405124c5bca5f47b6fe071
AndreiCIlies <116495791+AndreiCIlies@users.noreply.github.com>
2023-12-01 19:35:13 +0200

    Changed filepath in mainwindow.cpp

--- QtClient/QtClient/mainwindow.cpp
+++ QtClient/QtClient/mainwindow.cpp
@@ -8,7 +8,7 @@ MainWindow::MainWindow(QWidget *parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
-    QPixmap pix(":/Resource Files/Images/Background.jpg");
+    QPixmap pix("./Images/Background.jpg");
     int width = ui->label->width();
     int height = ui->label->height();
     ui->label->setPixmap(pix.scaled(width, height, Qt::KeepAspectRatio));
