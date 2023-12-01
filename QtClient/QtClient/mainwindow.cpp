18d90679f1e4de6bd006d15348656f5b4eea6b82
AndreiCIlies <116495791+AndreiCIlies@users.noreply.github.com>
2023-12-01 18:20:49 +0200

    Added Background on GUI

--- QtClient/QtClient/mainwindow.cpp
+++ QtClient/QtClient/mainwindow.cpp
@@ -1,11 +1,17 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <QMessageBox>
+#include <QPixmap>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    QPixmap pix("C:/Users/usER/Desktop/Anul_II/Semestrul_I/MC/Proiect/Background.jpg");
+    int width = ui->label->width();
+    int height = ui->label->height();
+    ui->label->setPixmap(pix.scaled(width, height, Qt::KeepAspectRatio));
 }
 
 MainWindow::~MainWindow()
