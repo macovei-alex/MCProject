7fbdb6afd42f0ddd635bf22bb2cc12865990d6a6
AndreiCIlies <116495791+AndreiCIlies@users.noreply.github.com>
2023-12-01 19:10:53 +0200

    Testing Background

--- QtClient/QtClient/mainwindow.cpp
+++ QtClient/QtClient/mainwindow.cpp
@@ -8,7 +8,7 @@ MainWindow::MainWindow(QWidget *parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
-    QPixmap pix("C:/Users/usER/Desktop/Anul_II/Semestrul_I/MC/Proiect/Background.jpg");
+    QPixmap pix(":/Resource Files/Images/Background.jpg");
     int width = ui->label->width();
     int height = ui->label->height();
     ui->label->setPixmap(pix.scaled(width, height, Qt::KeepAspectRatio));
