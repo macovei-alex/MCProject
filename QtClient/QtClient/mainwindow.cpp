ed2ef2a47ffab45cd79d9beb93d00ea0462bff45
Dutu Andrada <dutuandrada14@gmail.com>
2023-12-04 21:25:48 +0200

    added a login icon(we might change that) and a Layout for SignIn

--- QtClient/QtClient/mainwindow.cpp
+++ QtClient/QtClient/mainwindow.cpp
@@ -12,6 +12,10 @@ MainWindow::MainWindow(QWidget *parent)
     int width = ui->label->width();
     int height = ui->label->height();
     ui->label->setPixmap(pix.scaled(width, height, Qt::KeepAspectRatio));
+    QPixmap pix1(":Resource Files/Images/login_icon.png");
+    ui->label_login->setPixmap(pix1.scaled(100, 100, Qt::KeepAspectRatio));
+    ui->label_login->raise();
+
 }
 
 MainWindow::~MainWindow()
