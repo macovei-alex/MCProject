9e1e5f0632b5da9b64aa7cfff1059a6c89fbc652
macovei-alexandru <maccelmare@gmail.com>
2024-01-04 13:58:43 +0200

    added reference to client services

--- QtClient/QtClient/main.cpp
+++ QtClient/QtClient/main.cpp
@@ -5,12 +5,12 @@
 
 int main(int argc, char* argv[])
 {
-	// Find out the current directory
+	/*Find out the current directory
 	TCHAR buffer[256];
 	DWORD buffer_size = MAX_PATH;
 	DWORD length = GetCurrentDirectory(buffer_size, buffer);
 	std::wstring wstr{ buffer, length };
-	qDebug() << "*****FOLDER***** : " << QString::fromStdWString(wstr);
+	qDebug() << "*****FOLDER***** : " << QString::fromStdWString(wstr);*/
 
 
 	QApplication a(argc, argv);
